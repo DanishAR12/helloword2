00adec682df71d0da5399a30c2a3760f5dbfa6bb
DanishAR12 <160243905+DanishAR12@users.noreply.github.com>
2024-03-23 14:35:00 +0500

    updated index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 let greet = "Hello World";
 console.log(greet);
+let userName = "Danish AR";
+console.log(greet + " " + userName);
 export {};
--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
 let greet = "Hello World";
 
 console.log(greet);
+
+let userName = "Danish AR";
+
+console.log(greet + " " + userName);
